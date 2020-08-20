5ec78df4b175af052b5d9d4820ebecb309b56dff
William Desportes <williamdes@wdes.fr>
2020-08-20 11:23:39 +0200

    Replace Sami by Doctum
    Fixes: https://github.com/phpseclib/phpseclib/issues/1365
    Closes: https://github.com/phpseclib/phpseclib/issues/416
    Closes: https://github.com/phpseclib/phpseclib/issues/1506

---
+++ doctum.conf.php
@@ -0,0 +1,32 @@
+<?php
+
+// Prevent e.g. 'Notice: Constant MATH_BIGINTEGER_MONTGOMERY already defined'.
+class MyArrayStore extends Doctum\Store\ArrayStore
+{
+    public function removeClass(Doctum\Project $project, $name)
+    {
+        unset($this->classes[$name]);
+    }
+}
+
+$root = realpath(__DIR__) . '/';
+
+$iterator = Symfony\Component\Finder\Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in($root . 'phpseclib');
+
+$versions = Doctum\Version\GitVersionCollection::create($root)
+    ->add('1.0')
+    ->add('2.0')
+    ->add('master');
+
+return new Doctum\Doctum($iterator, [
+    'versions'             => $versions,
+    'title'                => 'phpseclib API Documentation',
+    'build_dir'            => $root . 'api.phpseclib.org/%version%',
+    'cache_dir'            => $root . 'cache/%version%',
+    'default_opened_level' => 2,
+    'store'                => new MyArrayStore,
+    'remote_repository'    => new Doctum\RemoteRepository\GitHubRemoteRepository('phpseclib/phpseclib', $root),
+]);
